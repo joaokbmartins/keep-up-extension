92a828d8cb38d9397a1fbf5492ab3f05f98d4c7f
João Martins <martins.joao1909@gmail.com>
2022-12-03 05:57:58 -0300

    fix: removed exit() from script

--- scripts/content-chrome-pages.js
+++ scripts/content-chrome-pages.js
@@ -1,19 +1,19 @@
 const article = document.querySelector("article");
 
-if (!article) exit();
+if (article) {
+  const text = article.textContent;
+  const wordMathRegExp = /[^\s]+/g;
+  const words = text.matchAll(wordMathRegExp);
 
-const text = article.textContent;
-const wordMathRegExp = /[^\s]+/g;
-const words = text.matchAll(wordMathRegExp);
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
 
-const wordCount = [...words].length;
-const readingTime = Math.round(wordCount / 200);
-const badge = document.createElement("p");
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min far from high way to HELL! Total words: ${wordCount}`;
 
-badge.classList.add("color-secondary-text", "type--caption");
-badge.textContent = `⏱️ ${readingTime} min far from high way to HELL! Total words: ${wordCount}`;
+  const heading = article.querySelector("h1");
+  const date = article.querySelector("time")?.parentNode;
 
-const heading = article.querySelector("h1");
-const date = article.querySelector("time")?.parentNode;
-
-(date ?? heading).insertAdjacentElement("afterend", badge);
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
